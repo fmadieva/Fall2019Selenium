6375cc93a16707164aaa111826d4ebc7b117489c
fmadieva <fariza.madieva@gmail.com>
2020-03-14 10:54:46 -0500

    reverse a string for unit testing

---
+++ src/test/java/com/automation/tests/day8/UnitTestPractice.java
@@ -0,0 +1,46 @@
+package com.automation.tests.day8;
+
+public class UnitTestPractice {
+
+    public static void main(String[] args) {
+        //unit test
+        // to check if our method works properly
+        //if assertion fails, that means our method doesn't work correctly
+        //
+        String expected = "cba";
+        String toReverse = "abc";
+        String actual = reverseString(toReverse);
+
+        verifyEquals(expected, actual);
+
+
+    }
+
+    public static boolean verifyEquals(String expected, String actual) {
+        if (expected.equals(actual)) {
+            System.out.println("TEST PASSED");
+            return true;
+        } else {
+            System.out.println("Test failed!!!");
+            System.out.println("Expected: " + expected);
+            System.out.println("Actual: " + actual);
+            return false;
+        }
+    }
+
+    /**
+     * This method stands for reversing strings.
+     *
+     * @param str to reverse
+     * @return reversed string
+     */
+    public static String reverseString(String str) {
+        String reversed = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reversed += str.charAt(i);
+        }
+        return reversed;
+    }
+
+
+}
