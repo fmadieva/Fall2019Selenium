87313f85210a1eb5ebc4783bf7d16f7b13c4f6fd
fmadieva <fariza.madieva@gmail.com>
2020-03-14 12:58:43 -0500

    Search Tests with TestNG annotations

--- src/test/java/com/automation/tests/day8/SearchTests.java
+++ src/test/java/com/automation/tests/day8/SearchTests.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -29,6 +30,8 @@ public class SearchTests {
             // if there is a text- print it
             if (!var.isEmpty()){
                 System.out.println("var = " + var);
+                //verify that every search result contains java
+                Assert.assertTrue(var.toLowerCase().contains("java"));
             }
         }
     }
@@ -46,3 +49,7 @@ public class SearchTests {
         driver.quit();
     }
 }
+
+
+//test without assertion is useless - what makes test => test
+//without assertion you can not understand test has passed or failed
