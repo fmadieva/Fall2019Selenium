5e89dbd0d246bfc3ab28e2df5cfdf9d6b6b72481
fmadieva <fariza.madieva@gmail.com>
2020-03-17 18:52:52 -0500

    registration form with 2 tests

--- src/test/java/com/automation/tests/practice/RegistrationForm.java
+++ src/test/java/com/automation/tests/practice/RegistrationForm.java
@@ -1,4 +1,5 @@
 package com.automation.tests.practice;
+
 import com.automation.utilities.BrowserUtils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
@@ -10,7 +11,9 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 public class RegistrationForm {
+
     private String URL = "http://practice.cybertekschool.com/registration_form";
     private WebDriver driver;
     // p tag name of success message
@@ -25,15 +28,18 @@ public class RegistrationForm {
     private By maleBy = By.cssSelector("input[value='male']");
     private By femaleBy = By.cssSelector("input[value='female']");
     private By otherBy = By.cssSelector("input[value='other']");
+
     private By dateOfBirthBy = By.name("birthday");
     private By departmentBy = By.name("department");
     private By jobTitleBy = By.name("job_title");
+
     //languages
     private By cplusplusBy = By.xpath("//label[text()='C++']/preceding-sibling::input");
     private By javaBy = By.xpath("//label[text()='Java']/preceding-sibling::input");
     private By javascriptBy = By.xpath("//label[text()='JavaScript']/preceding-sibling::input");
     //sign up button
     private By signUpBy = By.id("wooden_spoon");
+
     @Test
     public void test1(){
         driver.findElement(firstNameBy).sendKeys("Patrick");
@@ -42,17 +48,24 @@ public class RegistrationForm {
         driver.findElement(emailBy).sendKeys("test@email.com");
         driver.findElement(passwordBy).sendKeys("123455678");
         driver.findElement(phoneBy).sendKeys("234-123-1231");
+
         driver.findElement(maleBy).click();
         driver.findElement(dateOfBirthBy).sendKeys("01/02/1940");
+
         Select departmentSelect = new Select(driver.findElement(departmentBy));
         departmentSelect.selectByVisibleText("Department of Agriculture");
+
         Select jobTitleSelect = new Select(driver.findElement(jobTitleBy));
         jobTitleSelect.selectByVisibleText("SDET");
+
         driver.findElement(javaBy).click();
         driver.findElement(signUpBy).click();
+
         BrowserUtils.wait(5);
+
         String expected = "You've successfully completed registration!";
         String actual = driver.findElement(By.tagName("p")).getText();
+
         Assert.assertEquals(actual, expected);
     }
 
@@ -65,9 +78,12 @@ public class RegistrationForm {
     }
 
     @Test
-
-
-
+    public void verifyAlphabeticLettersOnlyTest(){
+        driver.findElement(firstNameBy).sendKeys("123");
+        BrowserUtils.wait(3);
+        WebElement warningMessage = driver.findElement(By.xpath("//small[text()='first name can only consist of alphabetical letters']"));
+        Assert.assertTrue(warningMessage.isDisplayed());
+    }
 
 
     @BeforeMethod
@@ -77,8 +93,10 @@ public class RegistrationForm {
         driver.get(URL);
         driver.manage().window().maximize();
     }
+
     @AfterMethod
     public void teardown(){
         driver.quit();
     }
+
 }
