c1cd35101d04f6615c45616a8ea470eabd2f68ea
fmadieva <fariza.madieva@gmail.com>
2020-03-24 20:58:48 -0500

    test our driver test with google.com

--- src/test/java/com/automation/utilities/ConfigurationReader.java
+++ src/test/java/com/automation/utilities/ConfigurationReader.java
@@ -35,3 +35,37 @@ public class ConfigurationReader {
     }
 
 }
+/*
+Creational patters
+-- singleton
+        This pattern involves a single class which is responsible to create an object
+        while making sure that only single object gets created
+            This class provides a way to access its only object which can be access directly without need to instantiate
+            the object of the class
+ the simplest design pattern
+
+
+ scripts don't require design pattern
+
+ Implementation
+
+
+ public class SingleObject{
+ // create an object of SingleObject
+ private static SingleObject  instance = new SingleObject();
+
+ // make the constructor private so that this class cannot be instantiated
+ private SingleObject(){}
+
+ // Get the only object available
+ public static SingleObject getInstance(){
+ return instance;
+ }
+
+ We need:
+ 1. static variable
+ 2. private constructor
+ 3.
+
+
+ */
