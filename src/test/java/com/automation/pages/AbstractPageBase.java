351d184e37ba9fac90e88d7e7e13936e0740d52e
fmadieva <fariza.madieva@gmail.com>
2020-03-29 13:44:27 -0500

    practice more OOP

--- src/test/java/com/automation/pages/AbstractPageBase.java
+++ src/test/java/com/automation/pages/AbstractPageBase.java
@@ -1,4 +1,3 @@
-
 package com.automation.pages;
 
 import com.automation.utilities.BrowserUtils;
@@ -7,7 +6,10 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 /**
  * This class will be extended by page classes
@@ -18,18 +20,30 @@ import org.openqa.selenium.support.PageFactory;
  */
 public abstract class AbstractPageBase {
     protected WebDriver driver = Driver.getDriver();
+    protected WebDriverWait wait = new WebDriverWait(driver, 15);
 
-    public AbstractPageBase(){
+    @FindBy(css = "#user-menu > a")
+    protected WebElement currentUser;
+
+    public AbstractPageBase() {
         PageFactory.initElements(driver, this);
     }
+
+
+    public String getCurrentUserName(){
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.visibilityOf(currentUser));
+        return currentUser.getText().trim();
+    }
+
 
     /**
      * Method for vytrack navigation. Provide tab name and module name to navigate
      * @param tabName, like Dashboards, Fleet or Customers
      * @param moduleName, like Vehicles, Vehicles Odometer and Vehicles Costs
      */
-    public void navigateTo(String tabName, String moduleName){
-        String tabNameXpath = "//span[@class='title title-level-1' and contains(text(),'"+tabName+"')]";
+    public void navigateTo(String tabName, String moduleName) {
+        String tabNameXpath = "//span[@class='title title-level-1' and contains(text(),'" + tabName + "')]";
         String moduleXpath = "//span[@class='title title-level-2' and text()='" + moduleName + "']";
 
         WebElement tabElement = driver.findElement(By.xpath(tabNameXpath));
@@ -40,7 +54,7 @@ public abstract class AbstractPageBase {
         BrowserUtils.wait(4);
 
         actions.moveToElement(tabElement).
-                pause(3000).
+                pause(2000).
                 click(moduleElement).
                 build().perform();
 
---
+++ src/test/java/com/automation/pages/activities/CalendarEventsPage.java
@@ -0,0 +1,36 @@
+
+package com.automation.pages.activities;
+
+import com.automation.pages.AbstractPageBase;
+import com.automation.utilities.BrowserUtils;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+
+public class CalendarEventsPage extends AbstractPageBase {
+
+
+    @FindBy(css = "[title='Create Calendar event']")
+    private WebElement createCalendarEvent;
+
+
+    @FindBy(className = "select2-chosen")
+    private WebElement owner;
+
+
+    public String getOwnerName(){
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.visibilityOf(owner));
+        return owner.getText().trim();
+    }
+
+    public void clickToCreateCalendarEvent(){
+        BrowserUtils.waitForPageToLoad(10);
+        wait.until(ExpectedConditions.elementToBeClickable(createCalendarEvent)).click();
+    }
+}
+
+
+//where we store the web element of current user name locator => in abstractPageBase ; because every page has that webelement
+//owner name locator =>  in calenderEventsPage since it is belong to calender event
