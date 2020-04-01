102b288554c5245f8a7eab24e9cb405975ff1a29
fmadieva <fariza.madieva@gmail.com>
2020-03-31 20:58:12 -0500

    screenshot

--- src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
+++ src/test/java/com/automation/tests/vytrack/login/NewLoginTests.java
@@ -1,20 +1,26 @@
 package com.automation.tests.vytrack.login;
 
 import com.automation.pages.LoginPage;
+import com.automation.tests.vytrack.AbstractTestBase;
 import com.automation.tests.vytrack.TestBase;
 import com.automation.utilities.BrowserUtils;
 import com.automation.utilities.Driver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class NewLoginTests extends TestBase {
+public class NewLoginTests extends AbstractTestBase {
 
 
     @Test
     public void verifyPageTitle(){
+        //test --> ExtentTest object
+        //we must add to every test at the beginning
+        //test = report.createTest("Test name");
+        test = report.createTest("Verify page title");
         LoginPage loginPage = new LoginPage();
         loginPage.login();
-        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboard");
+        Assert.assertEquals(Driver.getDriver().getTitle(), "Dashboa");
+        test.pass("Page title Dashboard was verified");
     }
 
     /**
